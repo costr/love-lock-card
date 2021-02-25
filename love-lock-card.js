ae330fef6ab7546a01ec8087f891840c7f4e9bff
Adam Costenbader <amcostenbader@gmail.com>
2021-02-25 13:50:42 -0600

    Update love-lock-card.js

--- love-lock-card.js
+++ love-lock-card.js
@@ -21,7 +21,7 @@ class LoveLockCard extends HTMLElement {
       "var(--ha-card-box-shadow, 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2))";
     this.style.borderRadius = "var(--ha-card-border-radius, 2px)";
     this.style.background = "var(--paper-card-background-color)";
-    this.style.display = "block";
+    this.style.display = "flex";
     // this.style.position = "relative"
 
     const root = this.shadowRoot;
